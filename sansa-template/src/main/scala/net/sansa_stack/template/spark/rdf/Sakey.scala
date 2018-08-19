0953f497948670c1dcbcd1339c97efb26d1aa181
robin-stenzel <robin.stenzel94@gmail.com>
2018-08-19 13:10:03 +0200

    added nNonKeyFinder algorithm and interface

--- sansa-template/src/main/scala/net/sansa_stack/template/spark/rdf/Sakey.scala
+++ sansa-template/src/main/scala/net/sansa_stack/template/spark/rdf/Sakey.scala
@@ -22,6 +22,7 @@ import shapeless.LowPriority.For
 import scala.collection.immutable.HashSet
 
 
+
 object Sakey extends App{
   
   //changes triples into (property, set of sets of subjects)
@@ -72,46 +73,75 @@ object Sakey extends App{
   .getOrCreate()
   
   //Sansa read in triples
+  /*
+  val input = "src/main/resources/page_links_simple.nt"
+  val lang = Lang.NTRIPLES
+  val triples = spark.rdf(lang)(input)
+  triples.collect().foreach(println)
+  */
+  
   val input = "src/main/resources/rdf.nt"
   val lang = Lang.RDFXML
   val triples = spark.rdf(lang)(input)
   
+
   triples.cache()
 
+  
+
+  var t0 = System.nanoTime()
   val finalMap = getFinalMap(triples)._1
+  var t1 = System.nanoTime()
+  println("Elapsed time: " + (t1 - t0)/1000000000 + "s cp1: FinalMap")
   
   
+  t0 = System.nanoTime()
   //flatten Set of Sets to one single Set
   val flatMap = finalMap.flatMapValues(identity).reduceByKey((x,y) => x.++(y))
- 
+  
+
   
   //Create all combinations of (property, set of subjects, property2, set2 of subjects)
   //remove every combination with intersections set lower than n
   val filteredQuadruple = flatMap.cartesian(flatMap)
   .filter(f => f._1._1 != f._2._1 && f._1._2.intersect(f._2._2).size >= 2 && f._1._1.hashCode() < f._2._1.hashCode())
   
+  t1 = System.nanoTime()
+  println("Elapsed time: " + (t1 - t0)/1000000000 + "s cp2: flattenedMap")
+
+
+  t0 = System.nanoTime()
   //create Graph out of quadruple
   val edges = filteredQuadruple.map { s => (s._1._1, s._1._1, s._2._1)}
   val tripleRDD = edges.map(f => Triple.create(f._1, f._2, f._3))  
   val graph = tripleRDD.asGraph()
+  
+  val mapVertexidToValue = mutable.HashMap.empty[VertexId, Node]
+  graph.vertices.foreach(f => mapVertexidToValue.+=(f._1 -> f._2))
+  println("map")
+  println(mapVertexidToValue)
 
 
   //collect all properties that don't appear in any edge and put them in the list of non keys
-  val nNonKeys= finalMap.keys.distinct().subtract(graph.vertices.values).collect()
-
+  val isolatedNNonKeys= finalMap.keys.distinct().subtract(graph.vertices.values).collect()
+  t1 = System.nanoTime()
+  println("Elapsed time: " + (t1 - t0)/1000000000 + "s cp3: GraphX Graph")
 
   
 
-  
+  t0 = System.nanoTime()
   //calculate CCs with GraphX
   val connectedComponents = graph.connectedComponents()
   val mappedCC = connectedComponents.triplets.map(_.toTuple).map{ case ((v1,v2), (v3,v4), n1) => (v2, UnDiEdge(v1,v3))}
-  
+  t1 = System.nanoTime()
+  println("Elapsed time: " + (t1 - t0)/1000000000 + "s cp4: ConnecteComponents")  
+
+  t0 = System.nanoTime()
   //aggregate edges of the same connected component
   val edgeSet = mutable.HashSet.empty[UnDiEdge[VertexId]]
   val edgeSeqOp = (s: mutable.HashSet[UnDiEdge[VertexId]], v: UnDiEdge[VertexId]) => s += v
   val edgeCombOp = (p1: mutable.HashSet[UnDiEdge[VertexId]], p2: mutable.HashSet[UnDiEdge[VertexId]])=> p1 ++= p2
-  
+
   //aggregate vertices of the same connected component
   val vertexSet = mutable.HashSet.empty[VertexId]
   val vertexSeq = (s: mutable.HashSet[VertexId], v: VertexId) => s += v
@@ -122,8 +152,12 @@ object Sakey extends App{
   
   //join Edges and vertices with the same CC
   val joinedCC = mappedCC.aggregateByKey(edgeSet)(edgeSeqOp, edgeCombOp).join(vert)
+  joinedCC.take(100).foreach(println)
   
+  t1 = System.nanoTime()
+  println("Elapsed time: " + (t1 - t0)/1000000000 + "s cp5: Scalax Graph")
   
+  t0 = System.nanoTime()
   //create scalax graph out of edge and vertex set (better suited for removing and adding edges)
   val graphCC = joinedCC.map(f 
       =>{
@@ -131,12 +165,23 @@ object Sakey extends App{
         val edges = f._2._1
         getMaxCliques(nodes, edges)
       })
-      
+  
   graphCC.collect().foreach(println)
   
+    t1 = System.nanoTime()
+  println("Elapsed time: " + (t1 - t0)/1000000000 + "s cp6: MaxCliques")
+  
+  t0 = System.nanoTime()
+  val nonKeysRDD = graphCC.map(f =>{
+        getNonKeys(f._1, 1, f._2)
+      })
+  nonKeysRDD.take(50).foreach(println)
+  
+  t1 = System.nanoTime()
+  println("Elapsed time: " + (t1 - t0)/1000000000 + "s cp7: NonKeyFinder")  
   //uses minFill to create a chordal graph and maxCardinanilityOrdering to extract cliques
-  //returns one set of Nodes that form a clique per connected component 
-  def getMaxCliques (nodes: mutable.HashSet[VertexId], edges: mutable.HashSet[GraphEdge.UnDiEdge[VertexId]]) :  mutable.HashSet[mutable.HashSet[VertexId]] = {
+  //returns one pair of (set of cliques, distinct nodes) per connected component 
+  def getMaxCliques (nodes: mutable.HashSet[VertexId], edges: mutable.HashSet[GraphEdge.UnDiEdge[VertexId]]) :  (mutable.HashSet[mutable.HashSet[Node]], mutable.HashSet[Node]) = {
     val g2 = Graph.from(nodes, edges)
     
     
@@ -186,11 +231,12 @@ object Sakey extends App{
       def compare(x: g3.NodeT, y: g3.NodeT): Int = getNodeCardinality(x) compare getNodeCardinality(y)
     }
     
-    //returns set of MaxCliques
-    def maxCardinalityList(): mutable.HashSet[mutable.HashSet[VertexId]] ={
+    //returns set of MaxCliques and set of distinct Nodes
+    def maxCardinalityList(): (mutable.HashSet[mutable.HashSet[Node]], mutable.HashSet[Node]) ={
       val sizeGraph = g3.nodes.size
       val listRemovedNodes = mutable.HashSet.empty[VertexId]
-      val setOfCliques = mutable.HashSet.empty[mutable.HashSet[VertexId]]
+      val setOfCliques = mutable.HashSet.empty[mutable.HashSet[Node]]
+      val nodesOfCC = mutable.HashSet.empty[Node]
       var x = 0
       for ( x  <- 1 to sizeGraph){ 
         val maxCardinalityNode = g3.nodes.max(new Ordering[g3.NodeT] {
@@ -203,17 +249,20 @@ object Sakey extends App{
 
         
         val clique = mutable.HashSet.empty[VertexId]
-        clique.+=(maxCardinalityNode.value)
+        clique += maxCardinalityNode.value
         clique ++= maxCardinalityNode.neighbors.map(_.value).filter(f => listRemovedNodes.contains(f))
-
-        setOfCliques += clique  
+        
+        
+        val cliquesAsValues = clique.map(f => mapVertexidToValue.get(f).get)
+        setOfCliques += cliquesAsValues
+        nodesOfCC ++= cliquesAsValues
         
         listRemovedNodes += maxCardinalityNode.value
 
           
       }
       //filter out subsets
-      setOfCliques.filter(f => setOfCliques.forall(p => !f.subsetOf(p) || p == f))
+      return (setOfCliques.filter(f => setOfCliques.forall(p => !f.subsetOf(p) || p == f)), nodesOfCC)
     }
 
     maxCardinalityList()
@@ -222,9 +271,99 @@ object Sakey extends App{
         
     
     }
+  
+  //interface between PNKs and NonKEyFinder, returns set of maximal nNonKeys
+  def getNonKeys (PNK: mutable.HashSet[mutable.HashSet[Node]], n: Int, nodeSet: mutable.HashSet[Node]) : mutable.HashSet[mutable.HashSet[Node]] = {
+    val finalMapDict = mutable.LinkedHashMap.empty[Node, mutable.HashSet[mutable.HashSet[Node]]]
+    val listProperties = mutable.ArrayBuffer[Node]()
+    val listSubjects = mutable.HashSet.empty[Node]
+    val filteredMap = finalMap.filter(f => nodeSet.contains(f._1))
+    filteredMap.toLocalIterator.foreach(f => {
+      if(nodeSet.contains(f._1)){
+        finalMapDict += f._1 -> f._2
+        listProperties += f._1
+        listSubjects ++= f._2.flatten
+        
+      }
+
+    })
+
+
+    val initCurInter = listSubjects
+    val initCurNKey = mutable.HashSet.empty[Node]
+    val initSeenInter = mutable.HashSet.empty[mutable.HashSet[Node]]
+    val initNonKeySet = mutable.HashSet.empty[mutable.HashSet[Node]]
+
+    nNonKeyFinder(listProperties(0), initCurInter, initCurNKey, initSeenInter, initNonKeySet, listProperties, finalMapDict, PNK, n, 0)
+    
+    //filter out subsets
+    initNonKeySet.filter(p => initNonKeySet.forall(p2 => !p.subsetOf(p2) || p == p2))
+    
+  }
     
     
 
+  //checks if currentNKey union pi
+  def uncheckedNonKeys (argCurNKey: mutable.HashSet[Node],
+      argNonKeySet: mutable.HashSet[mutable.HashSet[Node]],
+      argPNK: mutable.HashSet[mutable.HashSet[Node]]) : mutable.HashSet[mutable.HashSet[Node]]  = {
+      val result = mutable.HashSet.empty[mutable.HashSet[Node]]
+      argPNK.foreach(f => {
+        if (argCurNKey.subsetOf(f) && argNonKeySet.forall(p => !f.subsetOf(p))){
+          result.+=(f)
+        }
+      })
+      
+      return result
+    
+  }
+  
+  
+  //finds all n-Non-keys but not definitely all maximal
+  def nNonKeyFinder (pi : Node,
+      curInter : mutable.HashSet[Node],
+      curNKey : mutable.HashSet[Node],
+      seenInter : mutable.HashSet[mutable.HashSet[Node]],
+      nonKeySet : mutable.HashSet[mutable.HashSet[Node]],
+      properties : mutable.ArrayBuffer[Node],
+      accessfinalMap : mutable.LinkedHashMap[Node, mutable.HashSet[mutable.HashSet[Node]]],
+      PNK: mutable.HashSet[mutable.HashSet[Node]],
+      n : Int,
+      i : Int) : Unit = {
+
+    if(uncheckedNonKeys(curNKey + pi, nonKeySet, PNK).size > 0){
+      val piValue = accessfinalMap.get(pi).get
+      var selectedExceptionSet = piValue
+      piValue.foreach(f => {
+          if(curInter.subsetOf(f)){
+            var selectedExceptionSet = mutable.HashSet(mutable.HashSet(f))
+          } 
+        })
+
+        selectedExceptionSet.foreach(s => {
+          val newInter = s.intersect(curInter)
+          if(newInter.size > 1){
+            if(!seenInter.contains(newInter)){
+              val nvNKey = curNKey + pi
+
+              if(newInter.size > n){
+                nonKeySet += nvNKey
+              }
+              
+              if(i+1 < properties.size){
+                nNonKeyFinder(properties(i+1), newInter, nvNKey, seenInter, nonKeySet, properties, accessfinalMap, PNK, n, i+1)
+              }
+              
+            }
+          }
+          seenInter += newInter
+        })
+
+    }
+    if(i+1 < properties.size){
+      nNonKeyFinder(properties(i+1), curInter, curNKey, seenInter, nonKeySet, properties, accessfinalMap, PNK, n, i+1)
+    }
+  }
 }
   
   
--- sansa-template/src/main/scala/net/sansa_stack/template/spark/rdf/TripleReader.scala
+++ sansa-template/src/main/scala/net/sansa_stack/template/spark/rdf/TripleReader.scala
@@ -3,6 +3,13 @@ import java.net.URI
 
 import net.sansa_stack.rdf.spark.io.NTripleReader
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.SparkSession
+import java.net.{ URI => JavaURI }
+import scala.collection.mutable
+import org.apache.spark.graphx.Graph
+import net.sansa_stack.rdf.spark.io._
+import net.sansa_stack.rdf.spark.model.graph._
+import org.apache.jena.riot.Lang
 
 import scala.collection.mutable
 
@@ -11,16 +18,16 @@ object TripleReader {
   def main(args: Array[String]) {
     parser.parse(args, Config()) match {
       case Some(config) =>
-        run(config.in)
+        run()
       case None =>
         println(parser.usage)
     }
   }
 
-  def run(input: String): Unit = {
+  def run(): Unit = {
 
     val spark = SparkSession.builder
-      .appName(s"Triple reader example  $input")
+      .appName(s"Triple reader example  ")
       .master("local[*]")
       .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
       .getOrCreate()
@@ -29,11 +36,35 @@ object TripleReader {
     println("|        Triple reader example       |")
     println("======================================")
 
-    val triplesRDD = NTripleReader.load(spark, URI.create(input))
+    /*val triplesRDD = NTripleReader.load(spark, URI.create("src/main/resources/rdf.nt"))
 
     triplesRDD.take(5).foreach(println(_))
 
     //triplesRDD.saveAsTextFile(output)
+*/
+    val lang = Lang.NTRIPLES
+    val triples = spark.rdf(lang)("src/main/resources/data_sets/DB_NaturalPlace.nt")
+    triples.cache()
+      
+
+  
+    val t2 = System.nanoTime()
+    val graph = triples.asGraph()
+    graph.cache()
+    
+    graph.edges.take(5).foreach(println)
+    println("finish Graph")
+    val t3 = System.nanoTime()
+    
+    val t0 = System.nanoTime()
+    val cc = graph.connectedComponents()
+    
+     
+    
+    println(cc.triplets.take(5).foreach(println))
+    val t1 = System.nanoTime()
+    println("Elapsed time: " + (t3 - t2)/1000000000 + "s graph")
+    println("Elapsed time: " + (t1 - t0)/1000000000 + "s ConnecteComponents")
 
     spark.stop
 
